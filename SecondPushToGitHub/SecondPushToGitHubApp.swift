fbdc839e43d520ee03c47fceaf51dec43bc6a0d9
Adam Heidmann <adamdhm@yahoo.com>
2022-08-28 18:45:37 -0700

    Initial Commit

---
+++ SecondPushToGitHub/SecondPushToGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecondPushToGitHubApp.swift
+//  SecondPushToGitHub
+//
+//  Created by Adam Heidmann on 8/28/22.
+//
+
+import SwiftUI
+
+@main
+struct SecondPushToGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
