3a469c92cdd8885aa93beab36278a66e1c31564e
Adam Heidmann <adamdhm@yahoo.com>
2022-08-28 18:48:46 -0700

    changed text on line 12

--- SecondPushToGitHub/ContentView.swift
+++ SecondPushToGitHub/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("This is my second upload to GitHub!")
             .padding()
     }
 }
